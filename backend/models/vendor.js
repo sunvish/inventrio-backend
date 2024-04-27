c4a2da635207fd3f1eae644bd6d6b8238ba690ce
SUNVISH BHATT <suvishmunna@gmail.com>
2024-04-27 19:03:09 +0530

    first commit

---
+++ backend/models/vendor.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const vendorSchema = mongoose.Schema({
+  name: {
+    type: "String",
+    required: true,
+  },
+  shopName: {
+    type: "String",
+    required: true,
+  },
+  location: {
+    type: "String",
+    required: true,
+  },
+  merchantId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "merchant",
+  },
+});
+
+const vendor = mongoose.model("vendor", vendorSchema);
+
+export default vendor;
